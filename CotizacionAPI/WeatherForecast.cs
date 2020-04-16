0b62110b3c7069444a84fef9664542d57fb6741b
jcmatos97 <jcmatos97@outlook.com>
2020-04-15 20:43:11 -0400

    Second Commit

--- CotizacionAPI/WeatherForecast.cs
+++ CotizacionAPI/WeatherForecast.cs
@@ -11,5 +11,7 @@ namespace CotizacionAPI
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+        
+        public string ss {get;set;}
     }
 }
